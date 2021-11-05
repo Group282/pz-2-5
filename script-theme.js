2d891f498e911c7d8e3a9a1a2ba0b968ae976df2
Тарас <taras190789@gmail.com>
2021-11-05 09:08:15 +0200

    version 1.0.0

---
+++ script-theme.js
@@ -0,0 +1,8 @@
+const buttonTheme = document.querySelector('.button-theme');
+const calculator = document.querySelector('.calculator');
+const buttonFunction = document.querySelector('.button.function');
+
+function change () {
+  calculator.style.background = 'red';
+  buttonFunction.style.background = 'green';
+}
